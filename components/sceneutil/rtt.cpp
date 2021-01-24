5c4e6c3f576d6ce3efb5828b5d8192c236a3e4d1
madsbuvi <madssandvei@zoho.com>
2021-01-24 10:42:21 +0100

    Stereo friendly water
    (cherry picked from commit 0e22c55e48a7f965367d3d430c1bef5357b22748)

---
+++ components/sceneutil/rtt.cpp
@@ -0,0 +1,103 @@
+#include "rtt.hpp"
+
+#include <osg/Node>
+#include <osg/NodeVisitor>
+#include <osg/Texture2D>
+#include <osgUtil/CullVisitor>
+
+#include <components/settings/settings.hpp>
+
+namespace SceneUtil
+{
+    // RTTNode's cull callback
+    class CullCallback : public osg::NodeCallback
+    {
+    public:
+        CullCallback(RTTNode* group)
+            : mGroup(group) {}
+
+        void operator()(osg::Node* node, osg::NodeVisitor* nv) override
+        {
+            osgUtil::CullVisitor* cv = static_cast<osgUtil::CullVisitor*>(nv);
+            mGroup->cull(cv);
+        }
+        RTTNode* mGroup;
+    };
+
+    // RTT camera 
+    class RTTCamera : public osg::Camera
+    {
+    public:
+        RTTCamera()
+        {
+            setRenderOrder(osg::Camera::PRE_RENDER, 1);
+            setClearMask(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+            setRenderTargetImplementation(osg::Camera::FRAME_BUFFER_OBJECT);
+
+
+            unsigned int rttSize = Settings::Manager::getInt("rtt size", "Water");
+            setViewport(0, 0, rttSize, rttSize);
+
+            mColorBuffer = new osg::Texture2D;
+            mColorBuffer->setTextureSize(rttSize, rttSize);
+            mColorBuffer->setWrap(osg::Texture::WRAP_S, osg::Texture::CLAMP_TO_EDGE);
+            mColorBuffer->setWrap(osg::Texture::WRAP_T, osg::Texture::CLAMP_TO_EDGE);
+            mColorBuffer->setInternalFormat(GL_RGB);
+            mColorBuffer->setFilter(osg::Texture::MIN_FILTER, osg::Texture::LINEAR);
+            mColorBuffer->setFilter(osg::Texture::MAG_FILTER, osg::Texture::LINEAR);
+            attach(osg::Camera::COLOR_BUFFER, mColorBuffer);
+
+            mDepthBuffer = new osg::Texture2D;
+            mDepthBuffer->setTextureSize(rttSize, rttSize);
+            mDepthBuffer->setSourceFormat(GL_DEPTH_COMPONENT);
+            mDepthBuffer->setInternalFormat(GL_DEPTH_COMPONENT24);
+            mDepthBuffer->setWrap(osg::Texture::WRAP_S, osg::Texture::CLAMP_TO_EDGE);
+            mDepthBuffer->setWrap(osg::Texture::WRAP_T, osg::Texture::CLAMP_TO_EDGE);
+            mDepthBuffer->setSourceType(GL_UNSIGNED_INT);
+            mDepthBuffer->setFilter(osg::Texture::MIN_FILTER, osg::Texture::LINEAR);
+            mDepthBuffer->setFilter(osg::Texture::MAG_FILTER, osg::Texture::LINEAR);
+            attach(osg::Camera::DEPTH_BUFFER, mDepthBuffer);
+        }
+
+        osg::ref_ptr<osg::Texture2D> mColorBuffer;
+        osg::ref_ptr<osg::Texture2D> mDepthBuffer;
+    };
+
+    RTTNode::RTTNode()
+    {
+        addCullCallback(new CullCallback(this));
+    }
+
+    RTTNode::~RTTNode()
+    {
+    }
+
+    void RTTNode::cull(osgUtil::CullVisitor* cv)
+    {
+        auto* vdd = getViewDependentData(cv);
+        apply(vdd->mCamera);
+        vdd->mCamera->accept(*cv);
+    }
+
+    osg::Texture2D* RTTNode::getColorTexture(osgUtil::CullVisitor* cv)
+    {
+        return getViewDependentData(cv)->mCamera->mColorBuffer.get();
+    }
+
+    osg::Texture2D* RTTNode::getDepthTexture(osgUtil::CullVisitor* cv)
+    {
+        return getViewDependentData(cv)->mCamera->mDepthBuffer.get();
+    }
+
+    RTTNode::ViewDependentData* RTTNode::getViewDependentData(osgUtil::CullVisitor* cv)
+    {
+        if (mViewDependentDataMap.count(cv) == 0)
+        {
+            mViewDependentDataMap[cv].reset(new ViewDependentData);
+            mViewDependentDataMap[cv]->mCamera = new RTTCamera();
+            setDefaults(mViewDependentDataMap[cv]->mCamera.get());
+        }
+
+        return mViewDependentDataMap[cv].get();
+    }
+}
---
+++ components/sceneutil/rtt.hpp
@@ -0,0 +1,62 @@
+#ifndef OPENMW_RTT_H
+#define OPENMW_RTT_H
+
+#include <osg/Node>
+
+#include <map>
+
+namespace osg
+{
+    class Texture2D;
+}
+
+namespace osgUtil
+{
+    class CullVisitor;
+}
+
+namespace SceneUtil
+{
+    class RTTCamera;
+
+    /// @brief Implements per-view RTT operations.
+    /// @par With a naive RTT implementation, subsequent views of multiple views will overwrite the results of the previous views, leading to
+    ///     the results of the last view being broadcast to all views. An error in all cases where the RTT result depends on the view.
+    /// @par If using an RTTNode this is solved by mapping RTT operations to CullVisitors, which will be unique per view. This requires
+    ///     instancing one camera per view, and traversing only the camera mapped to that CV during cull traversals.
+    /// @par Camera settings should be effectuated by overriding the setDefaults() and apply() methods, following a pattern similar to SceneUtil::StateSetUpdater
+    /// @par When using the RTT texture in your statesets, it is recommended to use SceneUtil::StateSetUpdater as a cull callback to handle this as the appropriate
+    ///     textures can be retrieved during SceneUtil::StateSetUpdater::Apply()
+    class RTTNode : public osg::Node
+    {
+    public:
+        RTTNode();
+        ~RTTNode();
+
+        osg::Texture2D* getColorTexture(osgUtil::CullVisitor* cv);
+
+        osg::Texture2D* getDepthTexture(osgUtil::CullVisitor* cv);
+
+
+        /// Apply state - to override in derived classes
+        /// @note Due to the view mapping approach you *have* to apply all camera settings, even if they have not changed since the last frame.
+        virtual void setDefaults(osg::Camera* camera) {};
+
+        /// Set default settings - optionally override in derived classes
+        virtual void apply(osg::Camera* camera) {};
+
+        void cull(osgUtil::CullVisitor* cv);
+
+    private:
+        struct ViewDependentData
+        {
+            osg::ref_ptr<RTTCamera> mCamera;
+        };
+
+        ViewDependentData* getViewDependentData(osgUtil::CullVisitor* cv);
+
+        typedef std::map< osgUtil::CullVisitor*, std::unique_ptr<ViewDependentData> >  ViewDependentDataMap;
+        ViewDependentDataMap mViewDependentDataMap;
+    };
+}
+#endif
